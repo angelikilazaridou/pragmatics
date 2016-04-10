e26461d7b428ffe9f056ea1994c8d0fa7988d310
angelikilazaridou <aggeliki.lazaridou@gmail.com>
2016-04-10 19:29:22 +0200

    adding second player...not ready

--- model/models/Players.lua
+++ model/models/Players.lua
@@ -2,7 +2,7 @@ require 'dpnn'
 require 'nn'
 
 local player1 = require 'models.player1'
-require 'models.Oracle_P2'
+local player2 = require 'models.player2'
 
 local players, parent =  torch.class('nn.Players', 'nn.Module')
 
@@ -17,7 +17,7 @@ function players:__init(opt)
 
 	--defining the two players
 	self.player1 = player1.model(opt.game_size, opt.feat_size, opt.vocab_size, opt.hidden_size, opt.gpuid) 
-	self.player2 = nn.Oracle_P2(self.batch_size, self.vocab_size)	
+	self.player2 = player2.model(opt.game_size, opt.feat_size, opt.vocab_size, opt.hidden_size, opt.gpuid)
 	
 	if opt.gpuid == 0 then
 		-- categorical for selection of feature
@@ -38,8 +38,8 @@ function players:updateOutput(input)
 	-- input: 	
 	local im1a = input[1]
 	local im1b = input[2]
-	local ref2a = input[3]
-	local ref2b = input[4]
+	local im2a = input[3]
+	local im2b = input[4]
 	local temp = input[5]
 	
 	--player 1 receives 2 images --
@@ -49,8 +49,8 @@ function players:updateOutput(input)
 	self.sampled_feat = self.selection:forward({self.probs, temp})
 	
 	-- player 2 receives 2 refs and 1 feature and predicts L or R
-	self.prediction = self.player2:forward({ref2a, ref2b, self.sampled_feat})
-	--self.prediction = self.sampled_feat
+	self.prediction = self.player2:forward({im2a, im2b, self.sampled_feat})
+	
 	-- baseline
 	local baseline = self.baseline:forward(torch.CudaTensor(self.batch_size,1))
 
@@ -68,8 +68,8 @@ function players:updateGradInput(input, gradOutput)
 	-- input:       
         local im1a = input[1]
         local im1b = input[2]
-	local ref2a = input[3]
-	local ref2b = input[4]
+	local im2a = input[3]
+	local im2b = input[4]
         local temp = input[5]
 
 	-- ds
@@ -80,12 +80,12 @@ function players:updateGradInput(input, gradOutput)
 	--do not continue back-backpropagating through players 
 	self.baseline:backward(torch.CudaTensor(1,1),dbaseline)
 
-	--backprop through player 2 is not really possible, just need to generate a 0 gradient for the selection module
-	--the doutput_p2 are basically zeros
-	local dsampled_feat = torch.CudaTensor(self.batch_size, self.vocab_size):fill(0)
+	--backprop through player 2 
+	local dsampled_feat = self.player2:backward({im2a, im2b, self.sampled_feat}, dprediction)
 
 	-- backprop though selection
-	local dprobs = self.selection:backward({self.probs, temp}, dsampled_feat)	
+	local dprobs = self.selection:backward({self.probs, temp}, dsampled_feat)
+	
 	--backrprop through player 1
 	dummy = self.player1:backward({im1a, im1b},dprobs)
 
@@ -98,13 +98,15 @@ end
 
 function players:evaluate()
 	self.player1:evaluate()
+        self.player2:evaluate()
 	self.baseline:evaluate()
 	self.selection:evaluate()
 end
 
 function players:training()
 	self.player1:training()
-    self.baseline:training()
+        self.player2:training()
+        self.baseline:training()
 	self.selection:training()
 
 end
@@ -115,15 +117,18 @@ end
 
 function players:parameters()
   	local p1,g1 = self.player1:parameters()
-	local p2,g2 = self.baseline:parameters()
+        local p2,g2 = self.player2:parameters()
+	local p3,g3 = self.baseline:parameters()
 	
 	local params = {}
 	for k,v in pairs(p1) do table.insert(params, v) end
 	for k,v in pairs(p2) do table.insert(params, v) end
+	for k,v in pairs(p3) do table.insert(params, v) end
   
 	local grad_params = {}
  	for k,v in pairs(g1) do table.insert(grad_params, v) end
 	for k,v in pairs(g2) do table.insert(grad_params, v) end
+	for k,v in pairs(p3) do table.insert(grad_params, v) end
 
 	return params, grad_params
 end
---
+++ model/models/player2.lua
@@ -0,0 +1,65 @@
+require 'misc.Peek'
+require 'nngraph'
+require 'dp'
+
+local player2 = {}
+function player2.model(game_size, feat_size, vocab_size, hidden_size, gpu)
+
+
+	local shareList = {}
+	--read in inputs
+	local inputs = {}
+	local all_prop_vecs = {}
+	shareList[1] = {} --share mapping to property space
+
+	for i=1,game_size do
+		local image = nn.Identity()() --insert one image at a time
+		table.insert(inputs, image)
+		
+		--map images to some property space
+		local property_vec = nn.Linear(feat_size, vocab_size)(image)
+		table.insert(shareList[1],property_vec)
+
+		local p_t = property_vec
+		table.insert(all_prop_vecs,p_t)
+
+	end
+
+
+	-- Then convert to 3d -> batch_size x 2 x feat_size
+	local properties_3d = nn.View(2,-1):setNumInputDims(1)(nn.JoinTable(2)(all_prop_vecs))
+	-- convert to batch_size * feat_size x 2
+	local properties_3d_b = nn.View(-1,game_size)(nn.Transpose({2,3})(properties_3d))
+	
+	--hidden layer for discriminativeness
+	local hid = nn.Linear(game_size, hidden_size)(properties_3d_b)
+	hid =  nn.Sigmoid()(hid)
+	
+	--compute discriminativeness
+	local discr = nn.Linear(hidden_size,1)(hid)
+	
+	--reshaping to batch_size x feat_size
+	local result = nn.View(-1,vocab_size)(discr)
+	-- probabilities over discriminative
+	local probs = nn.SoftMax()(result)
+	--take out discriminative 
+    	local outputs = {}
+	table.insert(outputs, probs)
+	
+
+	local model = nn.gModule(inputs, outputs)
+
+	if gpu>=0 then model:cuda() end
+        -- IMPORTANT! do weight sharing after model is in cuda
+       	for i = 1,#shareList do
+               	local m1 = shareList[i][1].data.module
+               	for j = 2,#shareList[i] do
+                       	local m2 = shareList[i][j].data.module
+                       	m2:share(m1,'weight','bias','gradWeight','gradBias')
+               	end
+       	end
+   
+	return model
+end
+
+return player1
