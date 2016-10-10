6984893a1a7e488486480345d5521df364e24ec6
aggelikilazaridou <aggeliki.lazaridou@gmail.com>
2016-10-10 21:47:11 +0200

    small

--- model/run_experiments.sh
+++ model/run_experiments.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 game_size=( 2 )
-layers=( "probs" "fc" )
+layers=( "probs" )
 grounding=( 0 )
 viewpoints=( 1 0 )
-vocab_size=( 10 30 100 450 )
-sender=( "sender_simple" "sender_no_embeddings" "sender_conv")
-embedding_size=( 50 100 300 )
+vocab_size=( 10 100 )
+sender=( "sender_simple" "sender_no_embeddings" )
+embedding_size=( 50 )
 noise=( 0 1 )
 
 for g in ${game_size[@]}; do
@@ -17,7 +17,7 @@ for g in ${game_size[@]}; do
 	    for s in ${sender[@]}; do
 		for n in ${noise[@]}; do
 			for e in ${embedding_size[@]}; do
-		        	th train.lua  -batch_size 32 -comm_game v2 -comm_feat_size 300 -vocab_size ${w} -property_size 100 -comm_game_size ${g} -hidden_size 20 -id exp1 -gpuid -1 -temperature 10 -max_iters 50000 -gr_task v2 -gr_feat_size 300 -embedding_size_S ${e}  -embedding_size_R ${e} -grounding ${gr} -comm_sender ${s} -save_checkpoint_every 1000 -comm_viewpoints ${v} -comm_layer ${l} -comm_noise ${n} > logs_fb/grounding/RL/log_sender@${s}_embS@${e}_embR${e}_V@${w}_@${g}_l@${l}_gr@${gr}_v@${v}_n@${n}.txt &  
+		        	th train.lua  -batch_size 32 -comm_game v2 -comm_feat_size 300 -vocab_size ${w} -property_size 50 -comm_game_size ${g} -hidden_size 20 -id exp1 -gpuid -1 -temperature 10 -max_iters 50000 -gr_task v2 -gr_feat_size 300 -embedding_size_S ${e}  -embedding_size_R ${e} -grounding ${gr} -comm_sender ${s} -save_checkpoint_every 1000 -comm_viewpoints ${v} -comm_layer ${l} -comm_noise ${n} > logs_fb/grounding/RL/log_sender@${s}_embS@${e}_embR${e}_V@${w}_@${g}_l@${l}_gr@${gr}_v@${v}_n@${n}.txt &  
 			done
 		done
 	    done
